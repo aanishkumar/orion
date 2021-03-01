93dc2cf3afbfa3f1126bb23064302bf22ea54cd1
Alby Anishkumar <albsvarghese@gmail.com>
2021-03-01 13:41:01 +0000

    Format code

--- data-generator/db/CleanDb.py
+++ data-generator/db/CleanDb.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-con = sqlite3.connect("../hmdata.sqlite")
+con = sqlite3.connect("../data.sqlite")
 cur = con.cursor()
 cur.execute('DROP TABLE data')
 con.commit()
--- data-generator/db/LoadTable.py
+++ data-generator/db/LoadTable.py
@@ -1,8 +1,12 @@
-import sqlite3
 import csv
-conn = sqlite3.connect("../hmdata.sqlite")
+import sqlite3
+
+conn = sqlite3.connect("../data.sqlite")
 cur = conn.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS data (Email Id text,Prefix text,Name text,Birth Date text,Phone Number text,Additional Email Id text,Address text,Zip Code text,City text,State text,Country text,Year text,Time text,Link text ,Text text)''')
+cur.execute(
+    '''CREATE TABLE IF NOT EXISTS data (Email Id text,Prefix text,Name text,Birth Date text,Phone Number text,
+    Additional Email Id text,Address text,Zip Code text,City text,State text,Country text,Year text,Time text,
+    Link text ,Text text)''')
 
 a_file = open("data.csv")
 rows = csv.reader(a_file)
